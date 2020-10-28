7c69c88a1267c7328262be2babcea11acff7c769
Micael <micael.matheus@live.com>
2020-10-27 23:25:06 -0300

    update navbar

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Desenvolvendo UI Clone
 
-# Techs 
- 🐱‍🏍JavaScript
- ✨CSS
- 🐱‍🐉HTML
- ❤ReactJS
+## Techs 
+ 📌 JavaScript
+ 📌 CSS
+ 📌 HTML
+ 📌 ReactJS
 
 ## Instruções
 
 `git clone` https://github.com/micaelmths/ui-clone-fsss.git na sua pasta de projetos
 
 `npm i` para instalar as dependências
+
+## Rotas
+/
+/dashboard
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import { FaGraduationCap, FaCaretDown, FaGlobeAmericas, FaCogs } from 'react-icons/fa'
 
 import logoMenuFsss from '../../images/logoMenuFsss.png'
+import perfil from '../../images/perfil.png'
 
 import './styles.css'
 
 function Navbar() {
   return (
     <div className="container-navbar">
-      <img src={logoMenuFsss} alt=""/>
+      <img src={logoMenuFsss} alt="Faculdade Santíssimo Sacramento"/>
       <h3>SCIAWEB - DIS</h3>
-      <ul className="nav-left"></ul>
-      <ul className="nav-right"></ul>
+      <ul className="nav-left">
+        <li>
+          <Link>
+            <FaGraduationCap /> 
+            <p>Dados Acadêmicos</p> 
+            <FaCaretDown />
+          </Link>
+        </li>
+        <li>
+          <Link><FaGlobeAmericas /> <p>F.SS.S Virtual</p> <FaCaretDown /></Link>
+        </li>
+        <li>
+          <Link><FaCogs /> <p>Sistema</p> <FaCaretDown /></Link>
+        </li>
+      </ul>
+      <ul className="nav-right">
+        <p>Micael</p>
+        <img src={perfil} alt="Micael"/>
+      </ul>
     </div>
   );
 }
